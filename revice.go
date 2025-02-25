9b521f57297af205b338a597b57090091ea8f64d
霍一慷 <huoyikang@huoyikangdeMac-mini.local>
2025-02-25 18:23:47 +0800

    fix: add func to deal the fail

--- revice.go
+++ revice.go
@@ -5,6 +5,11 @@ import (
 	"log"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
 func main() {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
